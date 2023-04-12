9e74ffdebf61412a342018c744c507b9424679f4
Magnus Meseck <magnus.meseck@googlemail.com>
2023-04-12 17:01:39 +0200

    Add support for specifying files & folders

--- source/unitium.ts
+++ source/unitium.ts
@@ -13,16 +13,12 @@ export class TestRunner
 {
     async runTests()
     {
-        const testModules = await FastGlob(["./**/*.test.js", "./**/*.test.ts", "./**/*.spec.js", "./**/*.spec.ts"],
-        {
-            onlyFiles: true,
-            absolute: true,
-            ignore: ['**/node_modules/**']
-        });
+        const testModules = await this.getTestModules();
 
         //Every file/module may contain multiple Test suites. We load the module and start the tests immediately
         var moduleLoadPromises: Promise<Promise<TestSuite>[]>[] = [];
-        for (const module of testModules) {
+        for (const module of testModules)
+        {
             moduleLoadPromises.push(this.loadAndRunTestSuites(module));
         }
 
@@ -33,23 +29,64 @@ export class TestRunner
         //We await the finishing of all the running TestSuites
         const finishedTestSuites = await Promise.all(allRunningTestSuites);
 
-        for (const testSuite of finishedTestSuites) {
+        for (const testSuite of finishedTestSuites)
+        {
             testSuite.printTestResults();
         }
 
         console.log();
+        console.log("exiting");
 
-        if(finishedTestSuites.find((suite) => suite.failedTests.length > 0)) 
+        if (finishedTestSuites.find((suite) => suite.failedTests.length > 0))
             process.exitCode = 1;
     }
 
-    async loadAndRunTestSuites(file: string)
+    private async getTestModules()
+    {
+        const [executable, script, ...fileSystemReferences] = process.argv;
+
+        var globs;
+
+        if (fileSystemReferences.length)
+        {
+            globs = fileSystemReferences.map(fileSystemReference => this.transformAndAddGlobPatternIfFolder(fileSystemReference));
+        }
+        else
+        {
+            globs = ["./**/*.{test,spec}.{js,ts}"];
+        }
+
+        console.log(globs);
+
+        return await FastGlob(globs,
+            {
+                onlyFiles: true,
+                absolute: true,
+                ignore: ['**/node_modules/**']
+            });
+    }
+
+    private transformAndAddGlobPatternIfFolder(reference: string)
+    {
+        if (reference.endsWith(".js") || reference.endsWith(".ts"))
+        {
+            // We are dealing with a file and leave it untouched, only replacing windows slashes with unix slashes for fast-glob
+            return reference.replace("\\", "/");
+        }
+        else
+        {
+            //We assume we are dealing with a directory and need to add a glob pattern to it
+            return reference.replace("\\", "/") + (reference.endsWith("/") ? "" : "/") + "**/*.{test,spec}.{js,ts}";
+        }
+    }
+
+    private async loadAndRunTestSuites(file: string)
     {
         const module = await import(URL.pathToFileURL(file).href);
 
         const runningTestSuites = [];
 
-        for(const key in module)
+        for (const key in module)
         {
             const testSuite = new TestSuite(module[key]);
             const runningTestSuite = testSuite.run();
@@ -62,13 +99,13 @@ export class TestRunner
 
 class TestSuite
 {
-    public name:string;
-    private suite:any;
-    public tests:Test[];
+    public name: string;
+    private suite: any;
+    public tests: Test[];
     public isRunning = false;
     public isCompleted = false;
 
-    constructor(testSuiteConstructor:{ new() : any })
+    constructor(testSuiteConstructor: { new(): any })
     {
         this.suite = new testSuiteConstructor();
         this.name = Object.getPrototypeOf(this.suite).constructor.name;
@@ -90,8 +127,9 @@ class TestSuite
         const testFunctions = Object.getOwnPropertyNames(Object.getPrototypeOf(this.suite));
         const tests = [];
 
-        for (const testFunction of testFunctions) {
-            if(testFunction != "constructor")
+        for (const testFunction of testFunctions)
+        {
+            if (testFunction != "constructor")
             {
                 tests.push(new Test(testFunction));
             }
@@ -100,11 +138,12 @@ class TestSuite
         return tests;
     }
 
-    async run() : Promise<TestSuite>
-    {    
+    async run(): Promise<TestSuite>
+    {
         this.isRunning = true;
-        let testPromises = [];    
-        for (const test of this.tests) {
+        let testPromises = [];
+        for (const test of this.tests)
+        {
             await test.run(this.suite);
         }
 
@@ -122,7 +161,7 @@ class TestSuite
 
         console.log();
         console.group(`${this.name}`);
-        
+
         console.group(`Passed: ${passedTests.length}/${totalTestCount}`);
         passedTests.forEach(t => t.printTestResult());
         console.groupEnd();
@@ -142,17 +181,17 @@ class Test
     public isCompleted = false;
 
     constructor(
-        public testFunctionName:string
-    ){}
+        public testFunctionName: string
+    ) { }
 
-    async run(testSuiteObject:any)
+    async run(testSuiteObject: any)
     {
         this.isRunning = true;
         try
         {
             await testSuiteObject[this.testFunctionName]();
         }
-        catch(e:BaseError | any)
+        catch (e: BaseError | any)
         {
             this.error = e;
         }
@@ -162,7 +201,7 @@ class Test
 
     public printTestResult()
     {
-        if(!this.error)
+        if (!this.error)
         {
             console.log("✔️    " + camelToNormal(this.testFunctionName));
         }
@@ -170,18 +209,20 @@ class Test
         {
             const errorPosition = this.error.stack.split("\n")[1].trim();
             const filePosition = /\(((.*?)\:(\d+)\:(\d+))\)/.exec(errorPosition)!;
-            console.group("❌   " + camelToNormal(this.testFunctionName) + " --> " + this.error.name + ": " + (this.error.message || "unkown")  + " --> \"" + filePosition[1] + "\"");
+            console.group("❌   " + camelToNormal(this.testFunctionName) + " --> " + this.error.name + ": " + (this.error.message || "unkown") + " --> \"" + filePosition[1] + "\"");
             // console.log(this.error);
             console.groupEnd();
         }
     }
 }
 
-function camelToNormal(camelCaseString: string) {
+function camelToNormal(camelCaseString: string)
+{
     return camelCaseString.replace(/([a-z])([A-Z])/g, '$1 $2').toLowerCase();
 }
 
-interface BaseError {
+interface BaseError
+{
     name: string,
     message: string,
     stack: string
