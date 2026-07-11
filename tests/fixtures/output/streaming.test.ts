3c65bae08d98f46dc7f192e66bf9252c3e9ee9fe
Magnus Meseck <magnus.meseck@googlemail.com>
2026-07-11 03:04:12 +0200

    tests: Add tests which run artificially long
    - this allows us to verifiy the behaviour on the console

---
+++ tests/fixtures/output/streaming.test.ts
@@ -0,0 +1,50 @@
+import { Sequential } from "../../../source/test-suite-api.ts";
+
+const wait = (milliseconds: number) => new Promise(resolve => setTimeout(resolve, milliseconds));
+
+@Sequential
+export class SequentialOutputTests
+{
+    async runsFor1Second()
+    {
+        await wait(1_000);
+    }
+
+    async runsFor6Seconds()
+    {
+        await wait(6_000);
+    }
+
+    async runsFor2Seconds()
+    {
+        await wait(3_000);
+    }
+}
+
+export class ParallelOutputTests
+{
+    async runsFor10Milliseconds()
+    {
+        await wait(10);
+    }
+
+    async runsFor100Milliseconds()
+    {
+        await wait(100);
+    }
+
+    async runsFor1Second()
+    {
+        await wait(1_000);
+    }
+
+    async runsFor3Seconds()
+    {
+        await wait(3_000);
+    }
+
+    async runsFor10Seconds()
+    {
+        await wait(10_000);
+    }
+}
